054851c36333bac070bff51c38f52d591fd5cc0e
stefaniaszz <131990404+stefaniaszz@users.noreply.github.com>
2025-03-25 13:54:54 -0300

    ok
    ok

---
+++ scipts-python/desafios/desafio01.py
@@ -0,0 +1,3 @@
+======== DESAFIO 01 ========
+nome = input('Qual é o seu nome?')
+print('Olá', (nome), '! Prazer em te conhecer!')
---
+++ scipts-python/desafios/desafio03.py
@@ -0,0 +1,6 @@
+num1 = input('Primeiro número ')
+num1 = int(num1)
+num2 = input('Segundo número ')
+num2 = int(num2)
+soma = num1 + num2
+print('A soma é %d' % (soma))
